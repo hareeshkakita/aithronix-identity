b62275eefc080f65202e6725e18ceff1cbe944aa
hareeshkakita <hareesh.kssr@gmail.com>
2025-09-06 11:53:15 +0300

    Aithronix user identity microservice

---
+++ src/main/java/com/aithronix/aithronix_identity/AithronixIdentityApplication.java
@@ -0,0 +1,13 @@
+package com.aithronix.aithronix_identity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AithronixIdentityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AithronixIdentityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aithronix/aithronix_identity/AithronixIdentityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aithronix.aithronix_identity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AithronixIdentityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
